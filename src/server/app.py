dc16ec8d918037d3e9741aa1885b88c844f5f3d4
SALOMON KABONGO <salomon@aims.ac.za>
2020-12-13 16:29:56 +0100

    add an endpoint to get all languages, clean the code to separete with binary and change the backend name to server

---
+++ src/server/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import request, render_template
+# this is only for debug purpose
+# import ipdb
+import os
+from flask_restful import Api
+
+from utils import model_load
+from models.predict import Predicter
+from resources.translate import TranslateResource
+
+app = Flask(__name__)
+
+api = Api(app)
+
+api.add_resource(TranslateResource, '/translate')
+
+if __name__=='__main__':
+    app.run(port=5000, debug=True)
---
+++ src/server/models/translation.py
@@ -0,0 +1,16 @@
+class Translation:
+    def __init__(self, source, target, input, output) -> None:
+        super().__init__()
+        self.source = source
+        self.target = target
+        self.input = input
+        self.output = output
+
+    @property
+    def data(self):
+        return {
+            'source': self.source,
+            'target': self.target,
+            'input': self.input,
+            'output': self.output
+        }
