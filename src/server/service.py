32c31ee5dde7c4516ce91f0931ff52235c2b5cd6
Catherine Gitau <catherinegitau94@gmail.com>
2021-02-26 10:14:13 +0000

    fix conflict

--- src/server/service.py
+++ src/server/service.py
@@ -1,7 +1,7 @@
-from flask import Flask
+from flask import Flask, json, request
 from flask import jsonify
-from flask import request,render_template
-import json as json
+# from flask import request,render_template
+# import json as json
 
 
 from utils import model_load
@@ -19,22 +19,38 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 # pred = Predicter()
 
 @app.route('/translate',methods=['GET','POST'])
-@cross_origin(origin='*',headers=['Content-Type','Authorization'])
+@cross_origin(origin='*',headers=['Content-Type', 'Authorization'])
 def sentiment():
-    available_models_file = '../../data/external/available_models.tsv'
+    available_models_file = 'data/available_models.tsv'
 
     model_loader = model_load.MasakhaneModelLoader(available_models_file=available_models_file)
 
-    model_dir, config, lc = model_loader.download_model('sw')
+    model_dir, config, lc = model_loader.download_model('twi')
     # model_dir, config, lc = model_loader.download_model('ln')
 
-
+    print(request)
+    
     if request.method =='POST':
-        sentence = request.form['feedback']
-        # sentence = "Hello \t me"
+        #sentence = request.body.sentence
+
+        data = request.data
+        print(json.loads(data))
+
+        #print(jsonify(request.data))
+        # sentence = request.form['feedback']
+        #sentence = "Hello \t me"
+        sentence = json.loads(data)
 
         sentiment = Predicter().predict_translation(sentence, model_dir, lc)
 
-        return render_template('index.html',feedback=sentiment,sentiment_value=sentiment, predict=True)
+        
+        # feedback = {'feedback': 'this is feedback'}
+        return json_response(sentiment)
 
-    return render_template('index.html', predict=False)
+    return json_response({'error': 'kudo not found'}, 404)
+    #     return render_template('index.html',feedback=sentiment,sentiment_value=sentiment, predict=True)
+
+    # return render_template('index.html', predict=False)
+
+def json_response(payload, status=200):
+    return (json.dumps(payload), status, {'content-type': 'application/json'})
