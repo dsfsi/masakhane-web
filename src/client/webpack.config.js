eed694f1d89ea370e66bdd3edb13aece9857b7e9
Catherine <catherinegitau94@gmail.com>
2021-05-04 11:06:33 +0300

    added host config

--- src/client/webpack.config.js
+++ src/client/webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
     contentBase: path.resolve(__dirname, 'public'),
     open: true,
     clientLogLevel: 'silent',
+    host: '0.0.0.0',
     port: 3000,
     historyApiFallback: true,
     compress: true,
