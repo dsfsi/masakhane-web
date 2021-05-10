b8bf505921dcfcea42889f379bf9fdbf7e38fe67
Catherine <catherinegitau94@gmail.com>
2021-05-10 10:31:03 +0300

    added link to joeynmt

--- src/client/src/pages/Home.js
+++ src/client/src/pages/Home.js
@@ -12,7 +12,8 @@ function Home() {
         <br />
         <TranslateCard />
         <br />
-        <p style={{fontSize: 12, color: 'gray'}}>This is a community research project. Read more about it <span style={{color: 'blue'}}>here</span></p>
+        <p style={{fontSize: 12, color: 'gray'}}>This is a community research project and as such, <b style={{fontSize: 12, color: 'black'}}> this service is not a production system. Therefore, it should not be used for official translations</b>. Don't see your language and interested in training one up yourself? Go <span style={{color: 'blue'}}><a href="/about">here</a></span> to learn how to contribute a model! </p>
+        <p style={{fontSize: 12, color: 'gray'}}> The models are powered by <span style={{color: 'blue'}}><a href="https://joeynmt.readthedocs.io/en/latest/tutorial.html">JoeyNMT</a></span><span role="img" aria-label="koala">🐨</span>; a minimalist machine translation toolkit based on pytorch. </p>
       </Container>
     </div>
   );
