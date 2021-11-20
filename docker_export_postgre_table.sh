b63bc217b20a47f2faefccb04328b9941d58c59a
Salomon Prod Server <salomon@aims.ac.za>
2021-11-20 14:34:45 +0000

    rm feedback

--- docker_export_postgre_table.sh
+++
@@ -1,8 +0,0 @@
-CONTAINER="db"
-DB="masakhane"
-TABLE="feedback"
-FILE="feedback_data.csv"
-
-# docker-compose -f docker-compose.prod.yml exec -u psql ${CONTAINER} psql -d ${DB} -c "COPY ${TABLE} TO STDOUT WITH CSV HEADER " > ${FILE}
-
-docker-compose -f docker-compose.prod.yml exec db psql --username=masakhane --dbname=masakhane -c "COPY ${TABLE} TO STDOUT WITH CSV HEADER " > ${FILE}
