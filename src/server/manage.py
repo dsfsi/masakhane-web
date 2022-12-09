f1e319e6da4402e41fdd33154fa9db3b5b80db49
Zenthon <u20536951@tuks.co.za>
2022-12-09 12:45:55 +0200

    👔 ✨ (Server): Added logic for add & remove lang

--- src/server/manage.py
+++ src/server/manage.py
@@ -7,7 +7,10 @@ from core import masakhane, load_model
 from flask import current_app
 from core.models.language import Language
 
-import click, json, os, unittest
+import click
+import json
+import os
+import unittest
 
 # import flask_unittest
 
@@ -18,12 +21,13 @@ cli = FlaskGroup(masakhane)
 def create_db():
     db.create_all()
     db.session.commit()
-        
+
 
 @cli.command("init_models")
 def init_models():
     print(current_app.models)
     print(masakhane.models)
+
 
 @cli.command("clean")
 def clean():
@@ -36,44 +40,63 @@ def clean():
 @cli.command("all_languages")
 def all_languages():
     for lan in Language.query.all():
-        print(lan.to_json() )
+        print(lan.to_json())
+
 
 @cli.command("add_language")
 @click.argument('name_tag')
 def add_language(name_tag):
-    with open(os.environ.get('JSON', 
-    "./languages.json"), 'r') as f:
-            distros_dict = json.load(f)
+    try:
+        language = Language.query.filter_by(
+            src_tgt_dmn=name_tag).first_or_404()
+
+        print(language)
+        db.session.delete(language)
+        db.session.commit()
+    except Exception as e:
+        pass
+
+    with open(os.environ.get('JSON',
+                             "./languages.json"), 'r') as f:
+        distros_dict = json.load(f)
 
     languages_short_to_full = {}
     languages_full_to_short = {}
 
     for distro in distros_dict:
-        languages_short_to_full[distro['language_short'].lower()] = distro['language_en'].lower()
-        languages_full_to_short[distro['language_en'].lower()] = distro['language_short'].lower()
+        languages_short_to_full[distro['language_short'].lower(
+        )] = distro['language_en'].lower()
+        languages_full_to_short[distro['language_en'].lower(
+        )] = distro['language_short'].lower()
 
     source, target, domain = name_tag.split('-')
-    language = Language(src_tgt_dmn=name_tag, 
-                        source_target_domain = f"{languages_short_to_full[source]}-{languages_short_to_full[target]}-{domain}")
+    language = Language(src_tgt_dmn=name_tag,
+                        source_target_domain=f"{languages_short_to_full[source]}-{languages_short_to_full[target]}-{domain}")
+
     db.session.add(language)
     db.session.commit()
-    
+
 
 @cli.command("remove_language")
 @click.argument('name_tag')
 def remove_language(name_tag):
     source, target, domain = name_tag.split('-')
-    # Be carefull 
-    language = Language.query.filter_by(src_tgt_dmn=name_tag).first_or_404()
+    # Be carefull
+    try:
+        language = Language.query.filter_by(
+            src_tgt_dmn=name_tag).first_or_404()
 
-    print(language)
-    db.session.delete(language)
-    db.session.commit()
+        print(language)
+        db.session.delete(language)
+        db.session.commit()
+    except Exception as e:
+        pass
+
 
 @cli.command("tests")
 def tests():
     """ Runs the tests without code coverage """
-    
+
     loader = unittest.defaultTestLoader
     suite = unittest.TestSuite()
 
@@ -86,6 +109,7 @@ def tests():
         return 0
     return 1
     # return tests
+
 
 if __name__ == "__main__":
     cli()
