e22f4be9309d787211280cfb29e12473ca571471
espoir murhabazi <espoir.mur@gmail.com>
2023-03-07 16:00:46 +0000

    fix the deployment for the application

--- src/seldon_core_components/model_handlers.py
+++ src/seldon_core_components/model_handlers.py
@@ -6,10 +6,17 @@ from pathlib import Path
 
 class OptimizedM100Model:
     def __init__(self, model_path, src_lang, tgt_lang):
-        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+        self._tokenizer = AutoTokenizer.from_pretrained(model_path)
         self._model = ORTModelForSeq2SeqLM.from_pretrained(model_path)
-        self.pipeline = pipeline(f"translation_{src_lang}_to_{tgt_lang}", model=self.model, tokenizer=self.tokenizer)
+        self.pipeline = pipeline(f"translation_{src_lang}_to_{tgt_lang}", model=self._model, tokenizer=self._tokenizer)
 
     def predict(self, X):
         output = self.pipeline(X)
         return output
+    
+    def health_status(self):
+        text_to_translate = "Hello, my name is Espoir Murhabazi,  I am a Software Engineer from Congo DRC but living in UK"
+        translation = self.predict(text_to_translate)
+        assert len(translation) == 1, "health check returning bad translation"
+        assert translation[0].get("translation_text") is not None, "health check returning bad translation"
+        return translation[0].get("translation_text")
